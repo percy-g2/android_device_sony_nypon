300a9f7ae52d27e30e0673eb27c2414074a61b36
Percy Gahlot <percy@Root.(none)>
2013-06-07 18:43:47 +0530

    initial changes for cm10.1

---
+++ BoardConfig.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/sony/nypon/BoardConfigVendor.mk
+
+#inherit from the common montblanc definitions
+-include device/sony/montblanc-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/nypon/include
+
+TARGET_KERNEL_CONFIG := cm_montblanc_nypon_defconfig
+
+TARGET_ARCH= arm
+
+COMMON_GLOBAL_CFLAGS += -DSTE_HDMI
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 16
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_CMDLINE := device/sony/nypon/config/cmdline.txt
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATA_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FORCE_KERNEL_ADDRESS  := 0x00008000
+BOARD_MKBOOTIMG_ARGS := 0x01000000
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p14
+
+TARGET_OTA_ASSERT_DEVICE := LT22i,LT22a,nypon
