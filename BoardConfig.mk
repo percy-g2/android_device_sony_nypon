b3864e27bc28c99cccefe80e17d0dd9d324e670a
percy_g2 <gahlotpercy@gmail.com>
2013-07-25 04:20:02 +0530

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,25 +1,7 @@
-#
-# Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
-
-
-
 USE_CAMERA_STUB := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/nypon/bluetooth
 
 # inherit from the proprietary version
 -include vendor/sony/nypon/BoardConfigVendor.mk
@@ -29,21 +11,12 @@ USE_CAMERA_STUB := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/sony/nypon/include
 
-TARGET_KERNEL_CONFIG := cm_montblanc_nypon_defconfig
+TARGET_KERNEL_CONFIG := riogrande_nypon_defconfig
 
 COMMON_GLOBAL_CFLAGS += -DSTE_HDMI
 
-
-# Audio 
-COMMON_GLOBAL_CFLAGS += -DSONY_ICS_BLOBS 
-
-
 # Handling of EGL ICS blobs
 BOARD_EGL_NEEDS_LEGACY_FB := true
-
-#Sd card
-BOARD_HAS_SDCARD_INTERNAL := true
-BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p14
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
@@ -61,7 +34,6 @@ BOARD_MKBOOTIMG_ARGS := 0x01000000
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
-
-
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p14
 
 TARGET_OTA_ASSERT_DEVICE := LT22i,LT22a,nypon
